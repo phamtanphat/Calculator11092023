05cfe2a1c577b1d86598c6e9a81cdef62b9c9576
phamtanphat <phatdroid94@gmail.com>
2023-10-31 21:33:10 +0700

    anh xa

--- app/src/main/java/com/example/calculator11092023/MainActivity.kt
+++ app/src/main/java/com/example/calculator11092023/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.calculator11092023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    // Global scope
+    private lateinit var edtNumber1: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Local scope
+        // Lay gia trị từ resource
+        // Ánh xạ
+        edtNumber1 = findViewById(R.id.edit_text_number1)
+        edtNumber1.setText("Text")
     }
 }
